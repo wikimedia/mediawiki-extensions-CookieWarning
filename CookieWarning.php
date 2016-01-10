97c134e3978f536f3d61447de5c434dd57c49bc9
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-01-10 18:58:21 +0100

    Improve documentation of code and add test features (npm, composer)
    Also: Add license (MIT).
    
    Change-Id: Ia184b396b648ecb4ef30b40d13618e2ecf72294a

--- CookieWarning.php
+++ CookieWarning.php
@@ -4,8 +4,8 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['CookieWarning'] = __DIR__ . '/i18n';
 	/* wfWarn(
-		'Deprecated PHP entry point used for CookieWarning extension. Please use wfLoadExtension instead, ' .
-		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+		'Deprecated PHP entry point used for CookieWarning extension. Please use wfLoadExtension' .
+		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	); */
 	return true;
 } else {
--- includes/CookieWarning.hooks.php
+++ includes/CookieWarning.hooks.php
@@ -1,17 +1,32 @@
 <?php
 
 class CookieWarningHooks {
-	public static function onSkinTemplateOutputPageBeforeExec( SkinTemplate &$sk, &$tpl ) {
+	/**
+	 * SkinTemplateOutputPageBeforeExec hook handler.
+	 *
+	 * Adds the CookieWarning information bar to the output html.
+	 *
+	 * @param SkinTemplate $sk
+	 * @param QuickTemplate $tpl
+	 */
+	public static function onSkinTemplateOutputPageBeforeExec(
+		SkinTemplate &$sk, QuickTemplate &$tpl
+	) {
+		// Config instance of CookieWarning
 		$conf = ConfigFactory::getDefaultInstance()->makeConfig( 'cookiewarning' );
 		$moreLink = '';
+		// if a "more information" URL was configured, add a link to it in the cookiewarning
+		// information bar
 		if ( $conf->get( 'CookieWarningMoreUrl' ) ) {
 			$moreLink = Html::element(
 				'a',
 				array( 'href' => $conf->get( 'CookieWarningMoreUrl' ) ),
-				'Mehr Informationen'
+				$sk->msg( 'cookiewarning-moreinfo-label' )->text()
 			);
 		}
-		if ( self::showWarning( $sk ) ) {
+		// if the cookiewarning should be visible to the user, append the element to
+		// the head data.
+		if ( self::showWarning( $sk->getContext() ) ) {
 			$tpl->data['headelement'] .= Html::openElement(
 					'div',
 					array( 'class' => 'mw-cookiewarning-container' )
@@ -36,19 +51,36 @@ class CookieWarningHooks {
 		}
 	}
 
+	/**
+	 * BeforePageDisplay hook handler.
+	 *
+	 * Adds the required style and JS module, if cookiewarning is enabled.
+	 *
+	 * @param OutputPage $out
+	 */
 	public static function onBeforePageDisplay( OutputPage $out ) {
-		if ( !$out->getRequest()->getCookie( 'cookiewarning_dismissed' ) ) {
+		if ( self::showWarning( $out->getContext() ) ) {
 			$out->addModuleStyles( array( 'ext.CookieWarning.styles' ) );
 			$out->addModules( array( 'ext.CookieWarning' ) );
 		}
 	}
 
+	/**
+	 * Checks, if the CookieWarning information bar should be visible to this user on
+	 * this page.
+	 *
+	 * @param IContextSource $context
+	 * @return boolean Returns true, if the cookie warning should be visible, false otherwise.
+	 */
 	private static function showWarning( IContextSource $context ) {
 		$user = $context->getUser();
 		$conf = ConfigFactory::getDefaultInstance()->makeConfig( 'cookiewarning' );
 		if (
+			// if enabled in LocalSettings.php
 			$conf->get( 'CookieWarningEnabled' ) &&
+			// if not already dismissed by this user (and saved in the user prefs)
 			!$user->getBoolOption( 'cookiewarning_dismissed', false ) &&
+			// if not already dismissed by this user (and saved in the browser cookies)
 			!$context->getRequest()->getCookie( 'cookiewarning_dismissed' )
 		) {
 			return true;
