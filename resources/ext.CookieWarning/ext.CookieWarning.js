034d96ac81f059e0e1bd9991fe73c145e1e0f6f5
Fomafix <fomafix@googlemail.com>
2018-03-22 20:35:37 +0100

    Use saveOption from module 'mediawiki.api.options'
    The module 'mediawiki.api.options' is present since MediaWiki 1.25.
    
    Change-Id: I986afad78f8054596e0efe89aa9e663c6c123f03

--- resources/ext.CookieWarning/ext.CookieWarning.js
+++ resources/ext.CookieWarning/ext.CookieWarning.js
@@ -14,10 +14,8 @@
 		// the user preferences.
 		if ( !mw.user.isAnon() ) {
 			// try to save, that the cookiewarning was disabled, in the user preferences
-			new mw.Api().postWithToken( 'options', {
-				action: 'options',
-				change: 'cookiewarning_dismissed=1'
-			} ).fail( function ( code, result ) {
+			new mw.Api().saveOption( 'cookiewarning_dismissed', '1' )
+			.fail( function ( code, result ) {
 				// if it fails, fall back to the cookie
 				mw.log.warn( 'Failed to save dismissed CookieWarning: ' + code + '\n' + result.error + '. Using cookie now.' );
 				setCookie();
