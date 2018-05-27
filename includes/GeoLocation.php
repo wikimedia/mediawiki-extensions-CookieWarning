af9bac181cfa3f2c577b8c43e6b65bdf633623fd
Paladox <thomasmulhall410@yahoo.com>
2018-05-27 10:21:42 +0000

    Add support for using geoip2
    This means we won't need to use a external ip to be able to apply cookiewarning to certain countrys.
    
    Bug: T195695
    Change-Id: I61405f0022d4e7e3ef1c7f5a8996a6b996a933cd

--- includes/GeoLocation.php
+++ includes/GeoLocation.php
@@ -42,9 +42,34 @@ class GeoLocation {
 		if ( !IP::isValid( $ip ) ) {
 			throw new InvalidArgumentException( "$ip is not a valid IP address." );
 		}
-		if ( !$this->config->get( 'CookieWarningGeoIPServiceURL' ) ) {
+		if ( $this->config->get( 'CookieWarningGeoIp2' ) ) {
+			try {
+				$reader = new GeoIp2\Database\Reader( $this->config->get( 'CookieWarningGeoIp2Path' ) );
+				$record = $reader->city( $this->getIP() );
+				$this->countryCode = $record->country->isoCode;
+			} catch ( Exception $ex ) {
+				return null;
+			}
+		} elseif ( $this->config->get( 'CookieWarningGeoIPServiceURL' ) ) {
+			$requestUrl = $this->config->get( 'CookieWarningGeoIPServiceURL' );
+			if ( substr( $requestUrl, -1 ) !== '/' ) {
+				$requestUrl .= '/';
+			}
+			$json = Http::get( $requestUrl . $this->getIP(), [
+				'timeout' => '2'
+			] );
+			if ( !$json ) {
+				return false;
+			}
+			$returnObject = json_decode( $json );
+			if ( $returnObject === null || !property_exists( $returnObject, 'country_code' ) ) {
+				return false;
+			}
+			$this->countryCode = $returnObject->country_code;
+		} else {
 			return null;
 		}
+<<<<<<< HEAD
 		$requestUrl = $this->config->get( 'CookieWarningGeoIPServiceURL' );
 		if ( substr( $requestUrl, -1 ) !== '/' ) {
 			$requestUrl .= '/';
@@ -60,6 +85,90 @@ class GeoLocation {
 			return false;
 		}
 		$this->countryCode = $returnObject->country_code;
+=======
+>>>>>>> 67c7aa5... Add support for using geoip2
 		return true;
 	}
+}
+
+
+
+
+
+<?php
+    /**
+     * GeoLocation implementation
+     */
+    
+    /**
+     * Implements the GeoLocation class, which allows to locate the user based on the IP address.
+     */
+    class GeoLocation {
+        private $config;
+        private $countryCode;
+        
+        /**
+         * @param Config $config
+         */
+        public function __construct( Config $config ) {
+            $this->config = $config;
+        }
+        
+        /**
+         * Returns the country code, if the last call to self::locate() returned true. Otherwise, NULL.
+         *
+         * @return null|string
+         */
+        public function getCountryCode() {
+            return $this->countryCode;
+        }
+        
+        /**
+         * Tries to locate the IP address set with self::setIP() using the geolocation service
+         * configured with the $wgCookieWarningGeoIPServiceURL configuration variable. If the config
+         * isn't set, this function returns NULL. If the config is set, but the URL is invalid or an
+         * other problem occures which resulted in a failed locating process, this function returns
+         * false, otherwise it returns true.
+         *
+         * @param string $ip The IP address to lookup
+         * @return bool|null NULL if no geolocation service configured, false on error, true otherwise.
+         * @throws ConfigException
+         */
+        public function locate( $ip ) {
+            $this->countryCode = null;
+            if ( !IP::isValid( $ip ) ) {
+                throw new InvalidArgumentException( "$ip is not a valid IP address." );
+            }
+            if ( $this->config->get( 'CookieWarningGeoIp2' ) ) {
+                if ( file_exists( $this->config->get( 'CookieWarningGeoIp2Path' ) ) ||
+                    class_exists( 'GeoIp2\Database\Reader' ) ) {
+                    return null
+                }
+                try {
+                    $reader = new GeoIp2\Database\Reader( $this->config->get( 'CookieWarningGeoIp2Path' ) );
+                    $record = $reader->city( $ip );
+                    $this->countryCode = $record->country->isoCode;
+                } catch ( Exception $ex ) {
+                    return null;
+                }
+            } elseif ( $this->config->get( 'CookieWarningGeoIPServiceURL' ) ) {
+                $requestUrl = $this->config->get( 'CookieWarningGeoIPServiceURL' );
+                if ( substr( $requestUrl, -1 ) !== '/' ) {
+                    $requestUrl .= '/';
+                }
+                $json = Http::get( $requestUrl . $ip, [
+                                  'timeout' => '2'
+                                  ] );
+                if ( !$json ) {
+                    return false;
+                }
+                $returnObject = json_decode( $json );
+                if ( $returnObject === null || !property_exists( $returnObject, 'country_code' ) ) {
+                    return false;
+                }
+                $this->countryCode = $returnObject->country_code;
+            } else {
+                return null;
+            }
+        }
 }
