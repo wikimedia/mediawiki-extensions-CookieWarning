af2b2645f4fc36f7858a45173ae6f22926f080d4
Paladox <thomasmulhall410@yahoo.com>
2019-11-14 14:10:56 +0000

    Fix tests to be compatible with master again
    Change-Id: Iff670def50ab15054099f7a1ca3adc119bd877ce

--- tests/phpunit/includes/HooksTest.php
+++ tests/phpunit/includes/HooksTest.php
@@ -22,7 +22,7 @@ class HooksTest extends MediaWikiLangTestCase {
 	/**
 	 * @throws \MWException
 	 */
-	protected function setUp() {
+	protected function setUp() : void {
 		parent::setUp();
 		MessageCache::singleton()->enable();
 	}
