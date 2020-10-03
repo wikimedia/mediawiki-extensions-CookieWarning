8ef52cde5879b11de06ef451244fb0905ad8af6b
paladox <thomasmulhall410@yahoo.com>
2020-10-03 16:34:05 +0100

    Add support for using geoip2
    This means we won't need to use a external ip to be able to apply cookiewarning to certain countrys.
    
    Bug: T195695
    Change-Id: I61405f0022d4e7e3ef1c7f5a8996a6b996a933cd

---
+++ includes/HttpGeoIP2GeoLocation.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CookieWarning;
+
+use Exception;
+use GeoIp2\Database\Reader;
+use InvalidArgumentException;
+use Throwable;
+use Wikimedia\IPUtils;
+
+/**
+ * Implements the GeoIP2GeoLocation class, which allows to locate the user based on the IP address using geoip2.
+ */
+class HttpGeoIP2GeoLocation implements GeoLocation {
+	private $geoIP2;
+	private $locatedIPs = [];
+
+	/**
+	 * @param string $geoIP2
+	 */
+	public function __construct( $geoIP2 ) {
+		$this->geoIP2 = $geoIP2;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 * @param string $ip The IP address to lookup
+	 * @return string|null
+	 */
+	public function locate( $ip ) {
+		if ( !class_exists( 'Reader' ) ) {
+			throw new InvalidArgumentException( 'You must install the geoip2 composer package.' );
+		} elseif ( !file_exists( $geoIP2 ) ) {
+			throw new InvalidArgumentException( 'File specified in geoIP2 does not exist' );
+		}
+
+		if ( isset( $this->locatedIPs[$ip] ) ) {
+			return $this->locatedIPs[$ip];
+		}
+
+		if ( !IPUtils::isValid( $ip ) ) {
+			throw new InvalidArgumentException( "$ip is not a valid IP address." );
+		}
+
+		try {
+			$reader = new Reader( $this->geoIP2 );
+			$record = $reader->city( $ip );
+			$this->locatedIPs[$ip] = $record->country->isoCode;
+
+			return $this->locatedIPs[$ip];
+		} catch ( Exception | Throwable $ex ) {
+			return null;
+		}
+	}
+}
