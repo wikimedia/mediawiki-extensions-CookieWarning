6c29ee073718bf4b2f9b1aa980c9d4a63f1c4e5a
Hannes <hannes@octofox.de>
2021-03-23 10:46:58 +0100

    epic: Add cookie preferences to extension
    Four checkboxes for required, preference, statistic and marketing cookies are added to the cookie banner
    Those preferences are saved to a 'cookiewarning_preferences' cookie, and to the user options if the user is logged in
    
    Cookie settings are viewable and changeable in the user preferences
    
    Five new config values are added:
    - $wgCookieWarningUnsetCookies
      - Unsets all cookies that are not strictly required
    - $wgCookieWarningRequiredCookies
      - A list of cookie names that can't be unset
    - $wgCookieWarningPreferenceCookies
      - A list of cookie names that belong to the preference group
    - $wgCookieWarningStatisticCookies
      - A list of cookie names that belong to the statistic group
    - $wgCookieWarningMarketingCookies
      - A list of cookie names that belong to the marketing group
    
    All cookie groups are based on https://gdpr.eu/cookies/
    
    These groups will only have an effect if $wgCookieWarningUnsetCookies is true.
    
    If $wgCookieWarningUnsetCookies is set to true, each cookie name is checked against all four lists, if nothing was found, the cookie is unset.
    
    NOTE: Cookies that are set through javascript can still be set, but will be unset each page load
    
    Preferences from the preference cookie are saved in the database after a successful login
    
    The maintenance script `RemoveDismissedState` can be used to force the display of the cookiebanner
    
    Bug: T278219
    Change-Id: I5c60640f3499364ca7c0713a628dcd49b085293b

---
+++ includes/Hooks/UserHooks.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @file
+ */
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\CookieWarning\Hooks;
+
+use MediaWiki\Hook\UserLoginCompleteHook;
+use MediaWiki\MediaWikiServices;
+use RequestContext;
+
+class UserHooks implements UserLoginCompleteHook {
+
+	/**
+	 * Save the preferences found in the preference cookie to the database
+	 * Mainly used for when the user accepts the cookies and then logs in
+	 *
+	 * @inheritDoc
+	 */
+	public function onUserLoginComplete( $user, &$inject_html, $direct ) {
+		$context = RequestContext::getMain();
+		if ( $context === null || $context->getRequest()->getCookie( 'cookiewarning_preferences' ) === null ) {
+			return;
+		}
+
+		$prefix = MediaWikiServices::getInstance()->getMainConfig()->get( 'CookiePrefix' );
+
+		$decoded = json_decode( $context->getRequest()->getCookie( 'cookiewarning_preferences', $prefix ), true );
+		if ( $decoded === null ) {
+			return;
+		}
+
+		$manager = MediaWikiServices::getInstance()->getUserOptionsManager();
+		$manager->setOption(
+			$user,
+			'cookiewarning_cookies_required',
+			true
+		);
+		$manager->setOption(
+			$user,
+			'cookiewarning_cookies_preference',
+			( $decoded['preference'] === null ) ? false : (bool)$decoded['preference']
+		);
+		$manager->setOption(
+			$user,
+			'cookiewarning_cookies_statistic',
+			( $decoded['statistic'] === null ) ? false : (bool)$decoded['statistic']
+		);
+		$manager->setOption(
+			$user,
+			'cookiewarning_cookies_marketing',
+			( $decoded['marketing'] === null ) ? false : (bool)$decoded['marketing']
+		);
+	}
+}
