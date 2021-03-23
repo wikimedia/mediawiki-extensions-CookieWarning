dbbb3147ad43d46b326406ded5f1dff7c56acdaa
Hannes <hannes@octofox.de>
2021-03-23 10:46:58 +0100

    epic: Add cookie preferences to extension
    Four checkboxes for required, preference, statistic and marketing cookies are added to the cookie banner
    Those preferences are saved to a 'cookiewarning_preferences' cookie, and to the user options if the user is logged in
    
    Cookie settings are viewable and changeable in the user preferences
    
    Seven new config values are added:
    - $wgCookieWarningEnablePreferences
      - Set to true to enable granular choosing of consent for specific
        cookie groups
      - Default: false
    - $wgCookieWarningUnsetCookies
      - Unsets all cookies that are not strictly required
      - Default: false
    - $wgCookieWarningSetCookiesSessionOnly
      - Instead of unsetting all cookies, they are set to session only and
        samesite strict
    - $wgCookieWarningRequiredCookies
      - A list of cookie names that can't be unset
      - Default: `["cookiewarning_dismissed", "cookiewarning_preferences",
        "UserID", "UserName", "_session", "mwuser-sessionId"]`
    - $wgCookieWarningPreferenceCookies
      - A list of cookie names that belong to the preference group
      - Default: `["VEE", "forceHTTPS", "thanks-thanked", "Token"]`
    - $wgCookieWarningStatisticCookies
      - A list of cookie names that belong to the statistic group
    - $wgCookieWarningMarketingCookies
      - A list of cookie names that belong to the marketing group
    
    All cookie groups are based on https://gdpr.eu/cookies/
    
    These groups will only have an effect if $wgCookieWarningUnsetCookies is true.
    
    If $wgCookieWarningUnsetCookies is set to true, each cookie name is checked against all four lists, if nothing was found, the cookie is unset.
    
    NOTE: Cookies that are set through javascript can still be set, but will be unset each page load, if unset cookies ist true
    
    Preferences from the preference cookie are saved in the database after a successful login
    
    The maintenance script `RemoveDismissedState` can be used to force the display of the cookiebanner
    This will only only remove the 'dismissed' state, not already given
    constent options
    
    Bug: T278219
    Change-Id: I5c60640f3499364ca7c0713a628dcd49b085293b

---
+++ includes/CookieWarning.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @file
+ */
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\CookieWarning;
+
+use MediaWiki\MediaWikiServices;
+use User;
+use WebRequest;
+
+class CookieWarning {
+	/**
+	 * @return mixed|null
+	 */
+	public static function getConfig() {
+		return MediaWikiServices::getInstance()->getService( 'CookieWarning.Config' );
+	}
+
+	/**
+	 * Returns true if the provided cookie name is an allowed cookie
+	 * The name is checked against the list of required, preference, statistic and marketing cookie names and
+	 * if the user has enabled one of those groups
+	 *
+	 * @param string $cookieName
+	 * @param WebRequest $request
+	 * @param User $user
+	 * @return bool
+	 */
+	public static function isAllowedCookie( string $cookieName, WebRequest $request, User $user ): bool {
+		$config = self::getConfig();
+		$prefix = MediaWikiServices::getInstance()->getMainConfig()->get( 'CookiePrefix' );
+		$name = str_replace( $prefix, '', $cookieName );
+
+		$preferences = [
+			'required' => true,
+			'preference' => false,
+			'statistic' => false,
+			'marketing' => false,
+		];
+
+		if ( $user->isRegistered() ) {
+			$options = MediaWikiServices::getInstance()->getUserOptionsManager()->getOptions( $user );
+
+			$preferences['preference'] = $options['cookiewarning_cookies_preference'] ?? false;
+			$preferences['statistic'] = $options['cookiewarning_cookies_statistic'] ?? false;
+			$preferences['marketing'] = $options['cookiewarning_cookies_marketing'] ?? false;
+		} elseif ( $request->getCookie( 'cookiewarning_preferences', $prefix ) !== null ) {
+			$decoded = json_decode( $request->getCookie( 'cookiewarning_preferences', $prefix ), true );
+			if ( $decoded !== null ) {
+				$preferences['preference'] = $decoded['preference'] ?? false;
+				$preferences['statistic'] = $decoded['statistic'] ?? false;
+				$preferences['marketing'] = $decoded['marketing'] ?? false;
+			}
+		}
+
+		$allowed = false;
+
+		$requiredCookies = $config->get( 'CookieWarningRequiredCookies' );
+		$preferenceCookies = $config->get( 'CookieWarningPreferenceCookies' );
+		$statisticsCookies = $config->get( 'CookieWarningStatisticCookies' );
+		$marketingCookies = $config->get( 'CookieWarningMarketingCookies' );
+
+		MediaWikiServices::getInstance()->getHookContainer()->run( 'CookieListRetrieve', [
+			&$requiredCookies,
+			&$preferenceCookies,
+			&$statisticsCookies,
+			&$marketingCookies,
+		] );
+
+		if ( in_array( $name, $requiredCookies, true ) ) {
+			$allowed = true;
+		}
+
+		if ( $preferences['preference'] === true && in_array( $name, $preferenceCookies, true ) ) {
+			$allowed = true;
+		}
+
+		if ( $preferences['statistic'] === true && in_array( $name, $statisticsCookies, true ) ) {
+			$allowed = true;
+		}
+
+		if ( $preferences['marketing'] === true && in_array( $name, $marketingCookies, true ) ) {
+			$allowed = true;
+		}
+
+		MediaWikiServices::getInstance()->getHookContainer()->run( 'CookieAllowedCheck', [
+			&$allowed,
+			$cookieName,
+			$preferences
+		] );
+
+		return $allowed;
+	}
+}
