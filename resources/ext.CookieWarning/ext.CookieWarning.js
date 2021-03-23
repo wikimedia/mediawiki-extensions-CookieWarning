578a7cf3869594a2a09c4962a3ba7a20b39e4b42
Hannes <hannes@octofox.de>
2021-03-23 10:46:58 +0100

    epic: Add cookie preferences to extension
    Four checkboxes for required, preference, statistic and marketing cookies are added to the cookie banner
    Those preferences are saved to a 'cookiewarning_preferences' cookie, and to the user options if the user is logged in
    
    Cookie settings are viewable and changeable in the user preferences
    
    Five new config values are added:
    - $wgCookieWarningUnsetCookies
      - Unsets all cookies that are not strictly required
    - $wgCookieWarningRequiredCookies
      - A list of cookie names that can't be unset
    - $wgCookieWarningPreferenceCookies
      - A list of cookie names that belong to the preference group
    - $wgCookieWarningStatisticCookies
      - A list of cookie names that belong to the statistic group
    - $wgCookieWarningMarketingCookies
      - A list of cookie names that belong to the marketing group
    
    All cookie groups are based on https://gdpr.eu/cookies/
    
    These groups will only have an effect if $wgCookieWarningUnsetCookies is true.
    
    If $wgCookieWarningUnsetCookies is set to true, each cookie name is checked against all four lists, if nothing was found, the cookie is unset.
    
    NOTE: At this point no message is shown to the user that the banner needs to be accepted in order for login functionality to work properly when $wgCookieWarningUnsetCookies is true
    
    The login will succeed, but upon any action the user is logged out again
    
    Also NOTE: Cookies that are set through javascript can still be set, but will be unset each page load
    
    Preferences from the preference cookie are saved in the database after a successful login
    
    The maintenance script `RemoveDismissedState` can be used to force the display of the cookiebanner
    
    Bug: T278219
    Change-Id: I5c60640f3499364ca7c0713a628dcd49b085293b

--- resources/ext.CookieWarning/ext.CookieWarning.js
+++ resources/ext.CookieWarning/ext.CookieWarning.js
@@ -1,38 +1,63 @@
-( function ( mw, $ ) {
+( function ( mw ) {
+	var container = document.querySelector( '.mw-cookiewarning-container' ),
+		inputPreference = document.querySelector( '#cookiewarning-cookies-preference input' ),
+		inputStatistic = document.querySelector( '#cookiewarning-cookies-statistic input' ),
+		inputMarketing = document.querySelector( '#cookiewarning-cookies-marketing input' );
+
 	/**
 	 * Sets the cookie, that the cookiewarning is dismissed. Called,
 	 * when the api query to save this information in the user preferences,
 	 * failed for any reason, or the user is not logged-in.
 	 */
 	function setCookie() {
+		var preferenceData = {
+			required: true,
+			preference: typeof inputPreference.checked === 'undefined' ? false : inputPreference.checked,
+			statistic: typeof inputStatistic.checked === 'undefined' ? false : inputStatistic.checked,
+			marketing: typeof inputMarketing.checked === 'undefined' ? false : inputMarketing.checked
+		};
+
+		mw.hook( 'cookiewarning.preferences_set' ).fire( preferenceData );
+
 		mw.cookie.set( 'cookiewarning_dismissed', true );
+		mw.cookie.set( 'cookiewarning_preferences', JSON.stringify( preferenceData ) );
 	}
 
-	$( function () {
-		if ( mw.cookie.get( 'cookiewarning_dismissed' ) ) {
-			$( '.mw-cookiewarning-container' ).detach();
-		} else {
-			// Click handler for the "Ok" element in the cookiewarning information bar
-			$( '.mw-cookiewarning-container button' ).on( 'click', function ( ev ) {
-				// an anonymous user doesn't have preferences, so don't try to save this in
-				// the user preferences.
-				if ( !mw.user.isAnon() ) {
-					// try to save, that the cookiewarning was disabled, in the user preferences
-					new mw.Api().saveOption( 'cookiewarning_dismissed', '1' )
-						.fail( function ( code, result ) {
-							// if it fails, fall back to the cookie
-							mw.log.warn( 'Failed to save dismissed CookieWarning: ' + code + '\n' + result.error + '. Using cookie now.' );
-							setCookie();
-						} );
-				} else {
-					// use cookies for anonymous users
-					setCookie();
-				}
-				// always remove the cookiewarning element
-				$( '.mw-cookiewarning-container' ).detach();
-
-				ev.preventDefault();
-			} );
+	if ( mw.cookie.get( 'cookiewarning_dismissed' ) ) {
+		container.parentNode.removeChild( container );
+	} else {
+		// Click handler for the "Ok" element in the cookiewarning information bar
+		if ( container.querySelector( 'button' ) === null ) {
+			return;
 		}
-	} );
-}( mediaWiki, jQuery ) );
+
+		container.querySelector( 'button' ).addEventListener( 'click', function ( ev ) {
+			mw.hook( 'cookiewarning.dismissed' ).fire( true );
+			// an anonymous user doesn't have preferences, so don't try to save this in
+			// the user preferences.
+			if ( !mw.user.isAnon() ) {
+				// try to save, that the cookiewarning was disabled, in the user preferences
+				/* eslint-disable */
+				new mw.Api().saveOptions( {
+					cookiewarning_dismissed: '1',
+					cookiewarning_cookies_preference: typeof inputPreference.checked === 'undefined' ? false : inputPreference.checked,
+					cookiewarning_cookies_statistics: typeof inputStatistic.checked === 'undefined' ? false : inputStatistic.checked,
+					cookiewarning_cookies_marketing: typeof inputMarketing.checked === 'undefined' ? false : inputMarketing.checked
+				} )
+					.fail( function ( code, result ) {
+						// if it fails, fall back to the cookie
+						mw.log.warn( 'Failed to save dismissed CookieWarning: ' + code + '\n' + result.error + '. Using cookie now.' );
+						setCookie();
+					} );
+				/* eslint-enable */
+			} else {
+				// use cookies for anonymous users
+				setCookie();
+			}
+			// always remove the cookiewarning element
+			container.parentNode.removeChild( container );
+
+			ev.preventDefault();
+		} );
+	}
+}( mediaWiki ) );
