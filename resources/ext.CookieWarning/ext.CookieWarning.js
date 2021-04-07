3e349ba63d11919c86144cfff10dfa0599511b6c
Hannes <hannes@octofox.de>
2021-04-07 19:53:06 +0200

    refactor: Cleanup ext.CookieWarning.js
    Change-Id: Ie4ed997893187f29be43acf059135f83ce99b784

--- resources/ext.CookieWarning/ext.CookieWarning.js
+++ resources/ext.CookieWarning/ext.CookieWarning.js
@@ -1,4 +1,7 @@
-( function ( mw, $ ) {
+( function ( mw ) {
+	var container = document.querySelector( '.mw-cookiewarning-container' ),
+		options = {};
+
 	/**
 	 * Sets the cookie, that the cookiewarning is dismissed. Called,
 	 * when the api query to save this information in the user preferences,
@@ -8,31 +11,40 @@
 		mw.cookie.set( 'cookiewarning_dismissed', true );
 	}
 
-	$( function () {
-		if ( mw.cookie.get( 'cookiewarning_dismissed' ) ) {
-			$( '.mw-cookiewarning-container' ).detach();
-		} else {
-			// Click handler for the "Ok" element in the cookiewarning information bar
-			$( '.mw-cookiewarning-container button' ).on( 'click', function ( ev ) {
-				// an anonymous user doesn't have preferences, so don't try to save this in
-				// the user preferences.
-				if ( !mw.user.isAnon() ) {
-					// try to save, that the cookiewarning was disabled, in the user preferences
-					new mw.Api().saveOption( 'cookiewarning_dismissed', '1' )
-						.fail( function ( code, result ) {
-							// if it fails, fall back to the cookie
-							mw.log.warn( 'Failed to save dismissed CookieWarning: ' + code + '\n' + result.error + '. Using cookie now.' );
-							setCookie();
-						} );
-				} else {
-					// use cookies for anonymous users
-					setCookie();
-				}
-				// always remove the cookiewarning element
-				$( '.mw-cookiewarning-container' ).detach();
-
-				ev.preventDefault();
-			} );
+	if ( mw.cookie.get( 'cookiewarning_dismissed' ) ) {
+		container.parentNode.removeChild( container );
+	} else {
+		// Click handler for the "Ok" element in the cookiewarning information bar
+		if ( container.querySelector( 'button' ) === null ) {
+			return;
 		}
-	} );
-}( mediaWiki, jQuery ) );
+
+		container.querySelector( 'button' ).addEventListener( 'click', function ( ev ) {
+			mw.hook( 'cookiewarning.dismissed' ).fire( true );
+			// an anonymous user doesn't have preferences, so don't try to save this in
+			// the user preferences.
+			if ( !mw.user.isAnon() ) {
+				// try to save, that the cookiewarning was disabled, in the user preferences
+				/* eslint-disable */
+				options = {
+					cookiewarning_dismissed: '1',
+				}
+
+				new mw.Api().saveOptions( options )
+					.fail( function ( code, result ) {
+						// if it fails, fall back to the cookie
+						mw.log.warn( 'Failed to save dismissed CookieWarning: ' + code + '\n' + result.error + '. Using cookie now.' );
+						setCookie();
+					} );
+				/* eslint-enable */
+			} else {
+				// use cookies for anonymous users
+				setCookie();
+			}
+			// always remove the cookiewarning element
+			container.parentNode.removeChild( container );
+
+			ev.preventDefault();
+		} );
+	}
+}( mediaWiki ) );
